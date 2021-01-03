7b90ef563aecfdb0a9b22c2ff1b324b3e6dfc274
Lee Prout <wleeprout@gmail.com>
2021-01-03 17:34:17 -0500

    renamed columns in CDC data for database compatibility

--- app.py
+++ app.py
@@ -1,6 +1,16 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from os import environ
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = environ.get('DATABASE_URL', 'sqlite:///maternal.sqlite')
+
+db = SQLAlchemy(app)
+
+class MMR(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    State
+
 
 @app.route('/')
 def index():
@@ -9,5 +19,5 @@ def index():
 if __name__ == '__main__':
     app.run(debug=True)
 
-    
+
 
